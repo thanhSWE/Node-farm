28f79ab740019c1502484eae63a78926cdfc6c67
thanhSWE <thanhb2012142@student.ctu.edu.vn>
2023-09-03 11:57:37 +0700

    install nodemon, slugify

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const slugify = require("slugify");
+
 const replaceTemplate = require("./module/replaceTemplate");
 
 ////////////////////////////////////////FILE////////////
@@ -41,6 +43,8 @@ const templateProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+const slug = dataObj.map(el => slugify(el.productName, {lower : true}));
+
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
   //Overview
